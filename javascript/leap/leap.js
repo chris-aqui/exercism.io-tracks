b985d65276407e844e3c479605f4a06bf371899f
Christine <aqui.christine@gmail.com>
2018-11-11 11:51:07 -0500

    init other tracks

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,9 +1,18 @@
 // find the leap year
 export const isLeap = (year) => {
-  if (year % 4 === 0) {
+  if (year % 4 !== 0) {
     return false;
   }
   if (year % 4 === 0 && year % 100 !== 0) {
     return true;
+  }
+  if (year % 100 === 0 && year % 400 !== 0) {
+    return false;
+  }
+  if (year % 400 === 0) {
+    return true;
+  }
+  if (year % 200 === 0 && year % 400 !== 0) {
+    return false;
   }
 };
